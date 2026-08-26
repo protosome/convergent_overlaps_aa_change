7e4f7f87d78e4180477843354584560a26c63242
protosome <89164622+protosome@users.noreply.github.com>
2026-08-26 19:12:32 -0400

    Update S4PRED_WEIGHTS_URL to use HTTPS

--- overlap_runtime/config.py
+++ overlap_runtime/config.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 
-S4PRED_WEIGHTS_URL = "http://bioinfadmin.cs.ucl.ac.uk/downloads/s4pred/weights.tar.gz"
+S4PRED_WEIGHTS_URL = (
+    "https://bioinf.cs.ucl.ac.uk/downloads/s4pred/weights.tar.gz"
+)
 ALLOWED_PLOT_METRICS = ("Combined", "ESM_avg", "SS_avg", "Align1", "Align2", "Sub1", "Sub2")
 
 
